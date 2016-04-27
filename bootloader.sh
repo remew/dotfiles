127b2f75825d4abe577fa647b7e2a26aa8812373
remew <foolishremew@gmail.com>
2016-04-27 18:42:47 +0900

    download repository

--- bootloader.sh
+++ bootloader.sh
@@ -2,15 +2,34 @@
 
 GIT_URL="https://github.com/remew/dotfiles.git"
 TAR_URL="https://github.com/remew/dotfiles/archive/master.tar.gz"
-DOT_PATH=~/dotfiles
+DOT_PATH=$HOME/dotfiles
 
-function is_exists() {
+function exists() {
     which "$1" > /dev/null 2>&1
     return $?
+}
+
+function dotfiles_download() {
+    if exists "git"; then
+        git clone $GIT_URL
+    elif exists "curl" || exists "wget"; then
+        if exists "curl"; then
+            curl -L $TAR_URL
+        elif exists "wget"; then
+            wget -qO - $TAR_URL
+        fi | tar zxv
+        mv dotfiles-master dotfiles
+    else
+        echo "curl or wget is required"
+        exit 1
+    fi
+    cd dotfiles
+    ls -la
 }
 
 echo "==========================="
 echo "remew's dotfiles bootloader"
 echo "==========================="
 
+dotfiles_download
 
