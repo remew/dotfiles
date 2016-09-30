8a99955aba9c5dc43f3a26718db0c4f8124498bb
remew <foolishremew@gmail.com>
2016-09-30 16:04:45 +0900

    Change CP template.

--- .vim/template/template.cp.js
+++ .vim/template/template.cp.js
@@ -1,8 +1,11 @@
 'use strict';
 
 Main((input) => {
-    input = input.trim().split('\n');
 });
+
+function readLine(input, N = 1) {
+    return input.splice(0, N);
+}
 
 function Main(main) {
   let input = '';
@@ -11,5 +14,6 @@ function Main(main) {
   sin.resume();
   sin.setEncoding('utf8');
   sin.on('data', chunk => input += chunk);
-  sin.on('end', () => main(input));
+  sin.on('end', () => main(input.trim().split('\n').map(line => line.split(' '))));
 }
+
